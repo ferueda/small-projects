0e90fb06ca52640ef9510372e00a5d8571084eee
ferueda <fe.rueda@outlook.com>
2020-03-01 21:04:33 -0300

    added selected seats functionality

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -1,1 +1,32 @@
 const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.seat--occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+let ticketPrice = Number(movieSelect.value);
+
+// Update total and count
+function updateCountAndPrice() {
+  const selectedSeats = document.querySelectorAll('.row .seat--selected')
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = Number(e.target.value);
+  updateCountAndPrice();
+})
+
+// Seat click event
+container.addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('seat') && 
+    !e.target.classList.contains('seat--occupied')) {
+
+    e.target.classList.toggle('seat--selected');
+
+    updateCountAndPrice();
+  } 
+})
