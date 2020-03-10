0867f5a8bf50c934a3a1d39a827c1cf4953bef0c
ferueda <fe.rueda@outlook.com>
2020-03-09 22:08:22 -0300

    added post req and res from the server

--- data-selfie-app/index.js
+++ data-selfie-app/index.js
@@ -4,3 +4,14 @@ app.listen(3000, () => {
   console.log('Listening at 3000');
 });
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+app.post('/api', (req, res) => {
+  console.log('I got a request!');
+  console.log(req.body);
+  res.json({
+    status: 'success',
+    latitude: req.body.lat,
+    longitude: req.body.lon
+  });
+});
